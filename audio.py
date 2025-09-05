17795448ea50ee74b0bfe076e136ceccd8e71d83
yair <ydorantes@icarusoftware.com>
2025-09-05 16:04:39 -0600

    ...

--- audio.py
+++
@@ -1,8 +0,0 @@
-import whisper  # Load model: tiny, base, small, medium, large (larger = more accurate but heavier)
-
-model = whisper.load_model("base")  # Transcribe audio file (mp3, wav, m4a, etc.)
-print("Model is on device:", next(model.parameters()).device)
-
-result = model.transcribe("xd.mp3", language="es")
-print("Transcription:")
-print(result["text"])
--- main.py
+++ main.py
@@ -32,14 +32,8 @@ print(API_KEY, "***")
 
 @app.get("/")
 def read_root():
-    print("Hello World")
-    # model = whisper.load_model("base")  # Transcribe audio file (mp3, wav, m4a, etc.)
-    # print("Model is on device:", next(model.parameters()).device)
 
-    result = model.transcribe("xd.mp3", language="es")
-    print("Transcription:")
-    print(result["text"])
-    return {"Hello": result["text"]}
+    return {"Hello": "World"}
 
 
 @app.post("/summarize")
