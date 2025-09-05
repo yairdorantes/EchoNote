1f0bc47b8186ddd8d1ac91b1d2aaa8a8d1dc4fd7
yair <ydorantes@icarusoftware.com>
2025-09-05 15:51:25 -0600

    ...

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ load_dotenv()
 app = FastAPI()
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # print(f"Using device: {device}")
+model = whisper.load_model("base")
 WHATSAPP_URL = os.environ.get("WHATSAPP_URL")
 API_KEY = os.environ.get("API_KEY")
 MODEL_URL = os.environ.get("MODEL_URL")
@@ -32,8 +33,6 @@ print(API_KEY, "***")
 @app.get("/")
 def read_root():
     print("Hello World")
-    model = whisper.load_model("base")
-
     # model = whisper.load_model("base")  # Transcribe audio file (mp3, wav, m4a, etc.)
     # print("Model is on device:", next(model.parameters()).device)
 
